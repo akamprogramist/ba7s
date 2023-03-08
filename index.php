64cd1feff953693116149c99a1002b784977c4a1
Akam Programist <akadev404@gmail.com>
2023-03-09 00:30:07 +0300

    commits

--- index.php
+++ index.php
@@ -29,19 +29,7 @@ if (!empty($_SESSION["id"])) {
         <button type="submit" name="submit">Add Car</button>
     </form>
 
-    <!-- <form class="" action="" method="post" autocomplete="off">
-        <label for="name">Name : </label>
-        <input type="text" name="name" id="name" required value=""> <br>
-        <label for="username">Username : </label>
-        <input type="text" name="username" id="username" required value=""> <br>
-        <label for="email">Email : </label>
-        <input type="email" name="email" id="email" required value=""> <br>
-        <label for="password">Password : </label>
-        <input type="password" name="password" id="password" required value=""> <br>
-        <label for="confirmpassword">Confirm Password : </label>
-        <input type="password" name="confirmpassword" id="confirmpassword" required value=""> <br>
-        <button type="submit" name="submit">Register</button>
-    </form>
+
     <form class="" action="" method="post" autocomplete="off">
         <label for="name">Name : </label>
         <input type="text" name="name" id="name" required value=""> <br>
