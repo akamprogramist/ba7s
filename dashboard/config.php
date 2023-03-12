67face7acd53f2105dfe39797487ecac83d3e059
Akam Programist <akadev404@gmail.com>
2023-03-12 22:17:37 +0300

    commits

--- dashboard/config.php
+++ dashboard/config.php
@@ -1,3 +1,17 @@
-<?php
-session_start();
-$conn = mysqli_connect("localhost", "root", "", "reglog");
+
+ <?php
+    session_start();
+    $conn = mysqli_connect("localhost", "root", "", "reglog");
+
+    // $serverName = "AKAM";
+
+    // $connectionInfo = array("Database" => "akam");
+    // $conn = sqlsrv_connect($serverName, $connectionInfo);
+
+    // if ($conn) {
+    //     echo "Connection established.<br />";
+    // } else {
+    //     echo "Connection could not be established.<br />";
+    //     die(print_r(sqlsrv_errors(), true));
+    // }
+    ?>
